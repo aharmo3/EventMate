29b854069b32ba842f9e6011c962b8dbcd546937
aharmo3 <aharmo3@gmail.com>
2023-03-13 16:17:06 +0100

    Commiting MUI Tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 import TopNav from "./components/TopNav";
+import RegistrationForm from "./components/RegistrationForm";
+
 import "./App.css";
 
 function App() {
-  return <TopNav />;
+  return (
+    <>
+      <TopNav />
+      <RegistrationForm />
+    </>
+  );
 }
 
 export default App;
