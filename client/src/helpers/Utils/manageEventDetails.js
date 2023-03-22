d69c43a7ed84375727f80b62523bf6e826dd9f95
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-03-22 22:24:43 +0000

    not in use - might delete soon

--- client/src/helpers/Utils/manageEventDetails.js
+++ client/src/helpers/Utils/manageEventDetails.js
@@ -1,51 +1,63 @@
-import ClientAPI from "../ClientAPI";
+
+// --------------------------------------------------------------------------------------------
+                                //CURRENTLY NOT BEING USED
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
 
 
-export default async function manageEventDetails (objectids, object){
+// import ClientAPI from "../ClientAPI";
+
+
+// export default async function manageEventDetails (objectids, object){
+
+// for (let i of object){
+
+//     if (objectids.includes(i.id)){
+//    let eventID= i.id
+//    let eventExists= await ClientAPI.getEventDetails(eventID)
+//     if (eventExists.data !== null){
+//         return 
+//         } else{
+//             console.log("sending event details to database....")
+
+//             let evLocation = null;
+//             let evCurrency=null;
+//             let evStartPrice = null;
+//             let pLink = null;
+//             let genr= null;
+//             let subg = null;
+//             let evHost= null;
+//             let evType= null;
+
+//             if(i.eventLocation)
+//                 evLocation= i.eventLocation
+//             if(i.currency)
+//                 evCurrency = i.currency;
+//             if (i.startingPrice)
+//                 evStartPrice = i.startingPrice
+//             if (i.purchaseLink)
+//                 pLink = i.purchaseLink
+//             if (i.genre)
+//                 genr= i.genre
+//             if (i.subgenre)
+//                 subg= i.subgenre
+//             if (i.eventHost)
+//                 evHost= i.eventHost
+//             if (i.eventType)
+//                 evType = i.eventType
+            
 
 
 
-    if (objectids.includes(object.id)){
-   let eventID= object.id
-   let eventExists= await ClientAPI.getEventDetails(eventID)
-    if (eventExists.data !== null){
-        return 
-        } else{
-            console.log("sending event details to database....")
-
-            let evLocation ="";
-            let evCurrency="";
-            let evStartPrice = "";
-            let pLink = "";
-            let genr= "";
-            let subg = "";
-            let evHost= "";
-            let evType= "";
-
-            if(object.eventLocation){
-                evLocation= object.eventLocation
-            }if(object.currency){
-                evCurrency = object.currency;
-            }if (object.startingPrice){
-                evStartPrice = object.startingPrice
-            }if (object.purchaseLink){
-                pLink = object.purchaseLink
-            }if (object.genre){
-                genr= object.genre
-            }if (object.subgenre){
-                subg= object.subgenre
-            }if (object.eventHost){
-                evHost= object.eventHost
-            } if (object.eventType){
-                evType = object.eventType
-            }
-
-
-
-            console.log("the stuff getting sent to db ....",);
-             ClientAPI.addEventDetails( object.id, object.name, object.date , object.time , 
-                object.image, evLocation, object.venue, evCurrency, evStartPrice, pLink, genr, 
-                subg, evHost,  evType)
-        }
-    }
-}
+//             console.log("the stuff getting sent to db ....",);
+//          let result=  await  ClientAPI.addEventDetails( i.id, i.name, i.date , i.time , 
+//                 i.image, evLocation, i.venue, evCurrency, evStartPrice, pLink, genr, 
+//                 subg, evHost,  evType)
+//                 console.log("magage event deetsssssss", result)
+//                 return result;
+//         }
+//     }
+//     }
+// }
