5a7b15b184a42298548879e428027e1a0e0fae45
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-03-23 10:54:01 +0000

    Shows events by location

--- client/src/components/SearchEvents.jsx
+++ client/src/components/SearchEvents.jsx
@@ -1,131 +1,121 @@
 import React, { useState, useEffect } from "react";
 import { GetByLocTM } from '../helpers/EventsApi/GetByLocTM.jsx';
-import Checkbox from '@mui/material/Checkbox';
 import "./chooseEvents.css"
 import { TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Local from "../helpers/Local";
+import addEventsToDB from "../helpers/Utils/addEventsToDB.js";
+import LocationDropdown from "./Registration/LocationDropdown";
+import takeEventDetails from "../helpers/Utils/takeEventDetails";
+import Form from "./Form";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import EventCard from "./EventCard";
+import EventsDisplayModal from "./EventsDisplayModal.jsx";
 
 function SearchEvents() {
-      const [location, setLocation] = useState(); // save location
-      const [events, setEvents] = useState(); 
-      const [showEvents, setShowEvents] = useState(false);
-      const [showEdit, setShowEdit]= useState(false);
-      const [showTitle, setShowTitle]= useState(true);
-      const [checkedItems, setCheckedItems] = useState([]);
+  const navigate = useNavigate();
+  const userInfo = Local.getUser();
+  const userLocation = userInfo.location;
+  const [events, setEvents] = useState();
+  const [location, setLocation] = useState();
+  const [showEvents, setShowEvents] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalData, setModalData] = useState({});
    
-
-    //Loads with user's current country in DB     
+ //Loads with user's current country in DB when loading
     useEffect(() => {
-    getLocation();
-    getEvents(location)
+      getLocation();
     }, []);
-    
-    //Loads events when location changes   
-    useEffect(() => {
-    getEvents(location)
-    }, [location]);
-
-    async function getLocation(){
-        // fetche location from db
-        //placeholder below
-        setLocation("barcelona");
-    };
 
 
-    const handleChange = event => {
-        setLocation(event.target.value);
-      };
-    
+  //gets user location from local storage and sets it
+  async function getLocation() {
+    await getEvents(userLocation);
+  }
+  function handleOpenModal(res) {
+    setModalData(res);
+    setIsOpen(true);
+  }
+  const handleLocationChange = async (form) => {
+    await getEvents(form.location);
+    setLocation(form.location);
+    setShowEdit(!showEdit);
+    console.log("events set as:", events);
+  };
 
-    function handleEditButton(){
-        setShowEdit(true);
-        setShowTitle(false);
-    }  
+  async function getEvents(location) {
+    let userId = userInfo.userId;
+    let results = await GetByLocTM(location);
+    let otherResults = await results.map((result) => {
+      let eventdetails = takeEventDetails(result, location, userId);
+      return eventdetails;
+    });
+    console.log("other Results", otherResults);
 
-    async function handleSubmit(e){
-        e.preventDefault();
-        await getEvents(location)
-        console.log("events set as:", events)
-        setShowEdit(false);
-        setShowTitle(true);
-      };
-    
-     
-    async function getEvents(place){
-        let results = await GetByLocTM(location);
-        //formatting the object to only take what we need
-    let newResults= results.map((result) =>{
-        return {"id": result.id, 
-        "name":result.name, 
-        "image": result.images["0"].url, 
-        "date" : result.dates.start.localDate, 
-        "time" : result.dates.start.localTime, 
-        "venue" : result._embedded.venues["0"].name}});
-
-        console.log("new Results" , newResults)
-        await setEvents(newResults);
-        setShowEvents(true);
-    }
+    await setEvents(otherResults);
+    setShowEvents(true);
+  }
 
 
+    // async function handleSend() {
+    //   // let newEvents = await addEventsToDB(chosenEvents, events, userInfo.userId);
+    //   console.log("The detail events resp", newEvents);
+    //   navigate("/matched");
+    // }
 
-  return (
-    <div className="choose-events">
-
-{ showEdit &&
-       <div className="edit-location">
-        <form className= "edit-loc-form" onSubmit={e => handleSubmit(e)}>
-        <h2>Events in </h2>
-        <TextField id="standard-basic" label="city" variant="standard" 
-              placeholder="enter your location"
-              name="location"
-              type="text"
-              value={location}
-              onChange={e => handleChange(e)}
-            />
-
-
-          {/* <button className="edit" type="submit">
-            ✓
-          </button> */}
-         
-        </form>
-      </div>
-}
-
-    { showTitle &&
-      <div className="title"> 
-       <h2>Events in {location}</h2>
-       <button className="edit" onClick={(e)=> handleEditButton()}>✎</button>
-       </div> 
-    }
-
-     { showEvents &&
-      <div className="show-choose-events">
-       <div className="results-choose-events">
-      {
-      events.map(r => {
-          return <div key={r.id} className="event-items">
-              {/* <Checkbox
-                className="event-checkbox"
-                value={r.id}
-                inputProps={{
-                    'aria-label': 'Checkbox A',
+return (
+  <div>
+    <div>
+      <h2 className="choose-events">
+        Events Near&nbsp;
+        {!showEdit && (
+          <>
+            {location}
+            <IconButton color="secondary" aria-label="Search">
+              <EditIcon
+                onClick={() => {
+                  setShowEdit(!showEdit);
                 }}
-            /> */}
-            <img src= {r.image} alt="" className="event-img"/>
-            <p className="event-title">{r.name}</p>
-            <p className="event-date-time">{r.date} | {r.time}</p>
-            <p className="event-venue">{r.venue}</p>
-          </div>
-      }
-           )}
-         </div>
-         </div>
-         }
-
-
+              />
+            </IconButton>
+          </>
+        )}
+        {showEdit && (
+          <Form
+            formInitialValues={{ location: "" }}
+            onFormChange={handleLocationChange}
+          >
+            <LocationDropdown
+              label="Location"
+              name="location"
+              defaultValue={userLocation}
+            />
+          </Form>
+        )}
+      </h2>
     </div>
-  )
+    {showEvents && (
+      <div className="show-choose-events">
+        <div className="results-choose-events">
+          {events.map((r) => {
+            return (
+              <div key={r.id} className="event-items">
+                <EventCard r={r} modelOpen={handleOpenModal} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    )}
+    <EventsDisplayModal
+      isOpen={isOpen}
+      handleOpen={setIsOpen}
+      eventData={modalData}
+    />
+  </div>
+);
 }
 
-export default SearchEvents
+export default SearchEvents;
